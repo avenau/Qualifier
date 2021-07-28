4e9704a858f60d68f54e9115e4ae15b553c07fc5
aven au <aven.au@fdmgroup.com>
2021-07-28 14:55:05 +1000

    Added SkillLevel service, repo and controller

---
+++ backend/qualifier/src/main/java/com/fdm/qualifier/repository/SkillLevelRepository.java
@@ -0,0 +1,11 @@
+package com.fdm.qualifier.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fdm.qualifier.model.SkillLevel;
+
+@Repository
+public interface SkillLevelRepository extends JpaRepository<SkillLevel, Integer> {
+
+}
