f453d8c156f98380e36627320d43f91705cc7dff
aven au <aven.au@fdmgroup.com>
2021-07-28 14:27:17 +1000

    Added Placement Service, Repo and Controller

--- backend/qualifier/src/main/java/com/fdm/qualifier/repository/PlacementRepository.java
+++ backend/qualifier/src/main/java/com/fdm/qualifier/repository/PlacementRepository.java
@@ -1,9 +1,11 @@
 package com.fdm.qualifier.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fdm.qualifier.model.Placement;
 
 public interface PlacementRepository extends JpaRepository<Placement, Integer>{
-	
+	public List<Placement> findByLocation(String location);
 }
