fc59e8974d1b926b851b564ad0b18a9804bc5a5c
aven au <aven.au@fdmgroup.com>
2021-07-28 11:21:22 +1000

    Added Result Model

---
+++ backend/qualifier/src/main/java/com/fdm/qualifier/model/Results.java
@@ -0,0 +1,78 @@
+package com.fdm.qualifier.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Results {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int resultId;
+	private double mark;
+	private Trainee trainee;
+	private Quiz quiz;
+	private boolean passed;
+	
+	public Results() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Results(double mark, Trainee trainee, Quiz quiz, boolean passed) {
+		super();
+		this.mark = mark;
+		this.trainee = trainee;
+		this.quiz = quiz;
+		this.passed = passed;
+	}
+
+	public int getResultId() {
+		return resultId;
+	}
+
+	public void setResultId(int resultId) {
+		this.resultId = resultId;
+	}
+
+	public double getMark() {
+		return mark;
+	}
+
+	public void setMark(double mark) {
+		this.mark = mark;
+	}
+
+	public Trainee getTrainee() {
+		return trainee;
+	}
+
+	public void setTrainee(Trainee trainee) {
+		this.trainee = trainee;
+	}
+
+	public Quiz getQuiz() {
+		return quiz;
+	}
+
+	public void setQuiz(Quiz quiz) {
+		this.quiz = quiz;
+	}
+
+	public boolean isPassed() {
+		return passed;
+	}
+
+	public void setPassed(boolean passed) {
+		this.passed = passed;
+	}
+
+	@Override
+	public String toString() {
+		return "Results [resultId=" + resultId + ", mark=" + mark + ", trainee=" + trainee + ", quiz=" + quiz
+				+ ", passed=" + passed + "]";
+	}
+	
+	
+}
