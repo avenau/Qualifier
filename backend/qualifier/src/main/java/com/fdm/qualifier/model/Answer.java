4a7742e17f9eea5496e0d932c3c2cc22027618c5
aven au <aven.au@fdmgroup.com>
2021-07-28 11:00:06 +1000

    Added Answer Model

---
+++ backend/qualifier/src/main/java/com/fdm/qualifier/model/Answer.java
@@ -0,0 +1,71 @@
+package com.fdm.qualifier.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Answer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int answerId;
+	private String content;
+	@ManyToOne
+	private Question question;
+	private boolean correct;
+	
+	public Answer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Answer(String content, Question question, boolean correct) {
+		super();
+		this.content = content;
+		this.question = question;
+		this.correct = correct;
+	}
+
+	public int getAnswerId() {
+		return answerId;
+	}
+
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public boolean isCorrect() {
+		return correct;
+	}
+
+	public void setCorrect(boolean correct) {
+		this.correct = correct;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [answerId=" + answerId + ", content=" + content + ", question=" + question + ", correct="
+				+ correct + "]";
+	}
+	
+	
+
+}
