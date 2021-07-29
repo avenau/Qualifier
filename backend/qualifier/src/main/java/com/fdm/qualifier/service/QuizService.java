aebfcbaf226d5d3be1e0c466166e0aa76580f9a4
aven au <aven.au@fdmgroup.com>
2021-07-29 11:38:53 +1000

    Can now save quiz results

--- backend/qualifier/src/main/java/com/fdm/qualifier/service/QuizService.java
+++ backend/qualifier/src/main/java/com/fdm/qualifier/service/QuizService.java
@@ -3,6 +3,9 @@ package com.fdm.qualifier.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fdm.qualifier.model.Quiz;
+import com.fdm.qualifier.model.Result;
+import com.fdm.qualifier.model.Trainee;
 import com.fdm.qualifier.repository.AnswerRepository;
 import com.fdm.qualifier.repository.QuestionRepository;
 import com.fdm.qualifier.repository.QuizRepository;
@@ -23,6 +26,13 @@ public class QuizService {
 		this.quizRepo = quizRepo;
 		this.questionRepository = questionRepository;
 		this.answerRepository = answerRepository;
+	}
+	
+	public Result saveQuizResult(Quiz finishedQuiz, double mark, Trainee trainee) {
+		boolean passed = mark >=finishedQuiz.getPassingMark();
+		Result result = resultRepo.save(new Result(mark, trainee, finishedQuiz, passed));
+				
+		return result;
 	}
 
 }
