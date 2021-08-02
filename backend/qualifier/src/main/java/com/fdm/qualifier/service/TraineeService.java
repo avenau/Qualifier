5c7ee85667d54422cbdcbed1b5110de7c367ceaf
William Wood <wcw838@uowmail.edu.au>
2021-08-02 10:31:22 +1000

    Changed suggestSkill frontend to use react-bootstrap

--- backend/qualifier/src/main/java/com/fdm/qualifier/service/TraineeService.java
+++ backend/qualifier/src/main/java/com/fdm/qualifier/service/TraineeService.java
@@ -1,6 +1,5 @@
 package com.fdm.qualifier.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
---
+++ frontend/src/components/trainee/mySkills.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { } from "react-bootstrap";
+
+function MySkills() {
+    const axios = require('axios');
+
+
+
+    return (
+        <div>
+
+        </div>
+    );
+
+}
+
+export default MySkills;
