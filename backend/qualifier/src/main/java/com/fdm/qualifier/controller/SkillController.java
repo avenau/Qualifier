7e91586ccaa7145c5a87bf20dc1224d7dfe8694a
aven au <aven.au@fdmgroup.com>
2021-08-03 11:03:33 +1000

    Add Skills Working

--- backend/qualifier/src/main/java/com/fdm/qualifier/controller/SkillController.java
+++ backend/qualifier/src/main/java/com/fdm/qualifier/controller/SkillController.java
@@ -1,11 +1,19 @@
 package com.fdm.qualifier.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fdm.qualifier.model.Skill;
 import com.fdm.qualifier.service.SkillService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class SkillController {
 	private SkillService skillService;
 
@@ -13,6 +21,26 @@ public class SkillController {
 	public SkillController(SkillService skillService) {
 		super();
 		this.skillService = skillService;
+	}
+	
+	@PostMapping("/addSkill")
+	public Map<String, String> addSkill(@RequestBody String skillname) {
+		
+		skillname = skillname.substring(0, skillname.length() - 1);
+		System.out.println(skillname);
+		
+		Map<String, String> status = new HashMap<String, String>();
+		status.put("status", "failed");
+		if (skillService.skillExist(skillname)) {
+			status.put("status", "already exist");
+		} else {
+			status.put("status", "success");
+			skillService.save(new Skill(skillname));
+		}
+		
+		
+		return status;
+		
 	}
 
 }
--- frontend/src/components/skills/addSkillPage.js
+++ frontend/src/components/skills/addSkillPage.js
@@ -1,6 +1,76 @@
-
+import { useState, useEffect } from "react";
+import { InputGroup, FormControl, Button, Container, Card, Row, Alert } from 'react-bootstrap';
 
 function AddSkillPage() {
+
+    const axios = require('axios');
+    const [skill, setSkill] = useState("");
+    const [show, setShow] = useState(false);
+    const [alertColor, setAlertColor] = useState("danger");
+    const [alertMessage, setAlertMessage] = useState("already exist!")
+    useEffect(() => {
+
+    })
+
+    const addingSkill = (() => {
+        axios
+        .post('http://localhost:9999/addSkill', skill)   
+        .then((response) => {
+            let status = response.data.status;
+            console.log(status);
+            if (status =="already exist"){
+                console.log("ALRWEADYT");
+                setShow(true);
+                setAlertMessage(<p><strong>{skill}</strong> already exist!</p>);
+                setAlertColor("danger");
+            } else if (status == "success"){
+                setShow(true);
+                setAlertMessage(<p><strong>{skill}</strong> successfully added!</p>);
+                setAlertColor("success");
+            }
+        })
+        .catch()
+        .finally()
+
+    })
+
+    const textBoxOnChange = ((event) => {
+        setSkill(event.target.value);
+    })
+
+
+    return(
+        <Container className="d-flex justify-content-center">
+            
+            <Row className="d-flex justify-content-center">
+                <Alert show= {show} variant={alertColor} onClose={() => setShow(false)} dismissible>
+                     {alertMessage}
+                </Alert>
+                <InputGroup className="mb-3 pt-5 w-50">
+                    <FormControl
+                    placeholder="Enter Skill"
+                    aria-label="Adding New Skill"
+                    aria-describedby="basic-addon2"
+                    onChange={textBoxOnChange}
+                    />
+                    <Button variant="outline-secondary" onClick={addingSkill}>
+                        Add Skill
+                    </Button>
+                </InputGroup>
+
+                <Card>
+                    <Card.Header as="h5">Suggested Skills</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Special title treatment</Card.Title>
+                        <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </Row>
+        </Container>
+    )
 
 }
 export default AddSkillPage;
